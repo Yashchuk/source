91ec45bb233f58afecde138d85c61ce583235d84
Oleg <kiss_home@mail.ru>
2012-06-07 10:34:10 +0300

    Converting from decimal to binary number and output in file binaryoutput.h

--- task1.3.cpp
+++ task1.3.cpp
@@ -4,36 +4,30 @@
 */
 
 #include "stdafx.h"
-#include <iostream>
 
-using namespace std;
+/**
+* This file has a function that converts decimal to binary and show it
+* (include <iostream>, using std::cout, using std::endl).
+*/
+#include "binaryoutput.h"		
 
 /**
 *@brief		This function return 1 if two bits are different and 0 if they are equal
 */
-unsigned long BitsEqual(unsigned long uNum, unsigned short bit1, unsigned short bit2);
-
-/**
-*@brief		This function return how much bits of number are using
-*/
-int HowMuchBitsUsing(unsigned long uNum);
-
-/**
-*@brief		This function converts a number from decimal to binary number system and show its
-*/
-void DecToBin(unsigned long uNum);
+unsigned long BitsEqual(unsigned long int uNum, unsigned short bit1, unsigned short bit2);
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	unsigned long uNumber;
+	using std::cin;
+	unsigned long int uNumber;
 	unsigned short bit1, bit2;
 	cout << "Enter number: ";
 	cin >> uNumber;
 	cout << "You entered: ";
 	DecToBin(uNumber);
-	cout << "The bits numeretion begin form 0"
-		"\n(example dec - 14, bit - 1110. bit #0 - 0, bit #1 - 1, etc.)\n"
-		<< "Enter first bit: ";
+	cout << "The bits numeretion begin from 0\n"
+		"(example dec - 14, bit - 1110. bit #0 - 0, bit #1 - 1, etc.)\n"
+		"Enter first bit: ";
 	cin >> bit1;
 	cout << "Enter second bit: ";
 	cin >> bit2;
@@ -42,41 +36,11 @@ int _tmain(int argc, _TCHAR* argv[])
 	return 0;
 }
 
-unsigned long BitsEqual(unsigned long uNum, unsigned short bit1, unsigned short bit2)
+unsigned long BitsEqual(unsigned long int uNum, unsigned short bit1, unsigned short bit2)
 {
 	bit1 = (uNum >> bit1) & 0x1;
 	cout << "First entered bit = " << bit1 << endl;
 	bit2 = (uNum >> bit2) & 0x1;
 	cout << "Second entered bit = " << bit2 << endl;
 	return bit1 ^ bit2;
-}
-
-int HowMuchBitsUsing(unsigned long uNum)
-{
-	int count = 1;
-	while(uNum > 1)
-	{
-		uNum >>= 1;
-		++count;
-	}
-	return count;
-}
-
-void DecToBin(unsigned long uNum)
-{
-	
-	int count = HowMuchBitsUsing(uNum);
-	int bits = count;
-	int * ptemp = new int[count];
-	while(uNum > 1)
-	{
-		--count;
-		ptemp[count] = (uNum % 2) ;
-		uNum /= 2;
-	};
-	ptemp[--count] = uNum;
-	for(int i = 0; i < bits; ++i)
-		cout << ptemp[i];
-	cout << " (bin)" << endl;
-	delete [] ptemp;
 }
