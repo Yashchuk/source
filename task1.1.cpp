26f85c98d44ee3f901b8f23e6e8a6b777845100f
Oleg <kiss_home@mail.ru>
2012-06-07 10:33:20 +0300

    Converting from decimal to binary number and output in file binaryoutput.h

--- task1.1.cpp
+++ task1.1.cpp
@@ -4,28 +4,22 @@
 */
 
 #include "stdafx.h"
-#include <iostream>
 
-using namespace std;
+/**
+* This file has a function that converts decimal to binary and show it
+* (include <iostream>, using std::cout, using std::endl).
+*/
+#include "binaryoutput.h"
 
 /**
 *@brief		This function return next even number
 */
-unsigned long NextEvenNumber(unsigned long uNum);
-
-/**
-*@brief		This function return how much bits of number are using
-*/
-int HowMuchBitsUsing(unsigned long uNum);
-
-/**
-*@brief		This function converts a number from decimal to binary number system and show its
-*/
-void DecToBin(unsigned long uNum);
+unsigned long NextEvenNumber(unsigned long int uNum);
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	unsigned long uNumber;
+	using std::cin;
+	unsigned long int uNumber;
 	cout << "Enter number: ";
 	cin >> uNumber;
 	cout << "You entered: ";
@@ -38,37 +32,7 @@ int _tmain(int argc, _TCHAR* argv[])
 	return 0;
 }
 
-unsigned long NextEvenNumber(unsigned long uNum)
+unsigned long NextEvenNumber(unsigned long int uNum)
 {
 	return ( (uNum & (ULONG_MAX & ~0x1) ) + 2);
-}
-
-int HowMuchBitsUsing(unsigned long uNum)
-{
-	int count = 1;
-	while(uNum > 1)
-	{
-		uNum >>= 1;
-		++count;
-	}
-	return count;
-}
-
-void DecToBin(unsigned long uNum)
-{
-	
-	int count = HowMuchBitsUsing(uNum);
-	int bits = count;
-	int * ptemp = new int[count];
-	while(uNum > 1)
-	{
-		--count;
-		ptemp[count] = (uNum % 2) ;
-		uNum /= 2;
-	};
-	ptemp[--count] = uNum;
-	for(int i = 0; i < bits; ++i)
-		cout << ptemp[i];
-	cout << " (bin)" << endl;
-	delete [] ptemp;
 }
