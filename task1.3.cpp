beea7088e3c1a4c1e33164788581809b5cb927c1
Oleg <kiss_home@mail.ru>
2012-06-09 14:53:39 +0300

    final task1.3

--- task1.3.cpp
+++ task1.3.cpp
@@ -11,36 +11,52 @@
 */
 #include "binaryoutput.h"		
 
+typedef unsigned long int ULI;
+typedef unsigned short US;
+
 /**
 *@brief		This function return 1 if two bits are different and 0 if they are equal
 */
-unsigned long BitsEqual(unsigned long int uNum, unsigned short bit1, unsigned short bit2);
+bool BitsEqual(ULI uNum, US bit1, US bit2);
 
 int _tmain(int argc, _TCHAR* argv[])
 {
 	using std::cin;
-	unsigned long int uNumber;
-	unsigned short bit1, bit2;
+	const short MAX_BIT = sizeof(ULI) * 8 - 1;
+	ULI uNumber;
+	US bit1, bit2;
 	cout << "Enter number: ";
 	cin >> uNumber;
 	cout << "You entered: ";
 	DecToBin(uNumber);
 	cout << "The bits numeretion begin from 0\n"
 		"(example dec - 14, bit - 1110. bit #0 - 0, bit #1 - 1, etc.)\n"
-		"Enter first bit: ";
+		"Enter first bit (max 31): ";
 	cin >> bit1;
-	cout << "Enter second bit: ";
+	while(bit1 > MAX_BIT)
+	{
+		cout << "Try again, enter first bit (max " << MAX_BIT <<"): ";
+		cin >> bit1;
+	}
+	cout << "Enter second bit (max 31): ";
 	cin >> bit2;
-	cout << BitsEqual(uNumber, bit1, bit2) << endl;
-	system("@pause");
+	while(bit2 > MAX_BIT)
+	{
+		cout << "Try again, enter second bit (max " << MAX_BIT <<"): ";
+		cin >> bit2;
+	}
+	if(BitsEqual(uNumber, bit1, bit2))
+		cout << "Bits are different!\n";
+	else
+		cout << "Bits are equal!\n";
 	return 0;
 }
 
-unsigned long BitsEqual(unsigned long int uNum, unsigned short bit1, unsigned short bit2)
+bool BitsEqual(ULI uNum, US bit1, US bit2)
 {
-	bit1 = (uNum >> bit1) & 0x1;
+	bit1 = (US)( (uNum >> bit1) & (ULI) 0x1);
 	cout << "First entered bit = " << bit1 << endl;
-	bit2 = (uNum >> bit2) & 0x1;
+	bit2 = (US)( (uNum >> bit2) & (ULI) 0x1);
 	cout << "Second entered bit = " << bit2 << endl;
-	return bit1 ^ bit2;
+	return (bool) bit1 ^ (bool) bit2;
 }
