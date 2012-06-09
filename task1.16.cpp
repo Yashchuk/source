7c2007b6572f6b8b3ea5d1e264ac8d9758122e72
Oleg <kiss_home@mail.ru>
2012-06-09 14:53:17 +0300

    final task1.16

--- task1.16.cpp
+++ task1.16.cpp
@@ -19,25 +19,36 @@ typedef unsigned short US;
 *@brief		This function returns the unsigned long integer that contains 
 *			the block of 1 bits of length n, starting from the p-th bits
 */
-ULI BlockOf1Bits(US & pos, US & len);
+ULI BlockOf1Bits(US pos, US len);
 
 int _tmain(int argc, _TCHAR* argv[])
 {
 	using std::cin;
+	const int MAX_POS = sizeof(ULI) * 8 - 1;
 	US position, length;
 	cout << "Enter the position to start: ";
 	cin >> position;
+	while(position > MAX_POS)
+	{
+		cout << "Position can't be > " << MAX_POS << ", try again: ";
+		cin >> position;
+	}
 	cout << "Enter block length: ";
 	cin >> length;
+	while(length > MAX_POS - position)
+	{
+		cout << "Length can't be > " << MAX_POS - position << ", try again: ";
+		cin >> length;
+	}
 	DecToBin(BlockOf1Bits(position, length));
 	system("@pause");
 	return 0;
 }
 
-ULI BlockOf1Bits(US & pos, US & len)
+ULI BlockOf1Bits(US pos, US len)
 {
 	len += pos;
-	ULI number = ((~0 >> pos) << pos) ^ (0x1 << len);
+	ULI number = (ULI) ( ( (ULI) ~0 >> pos) << pos) ^ ( (ULI) 0x1 << len);
 	len = ~len;
-	return ((number << len) >> len);
+	return ( (number << len) >> len);
 }
