6cd327afffdc5b9b37614f0ef7d1354320d01252
Oleg <kiss_home@mail.ru>
2012-06-19 21:50:41 +0300

    declarations of Sort functions. taks3.26

---
+++ task3.26/Sorting.h
@@ -0,0 +1,29 @@
+/**
+* @file		Sorting.h
+* @brief	This file contains source code of Task 3.26.
+*			It contains declarations of Sort functions.
+*
+* Copyright 2012 by Oleg Kiss,
+*
+* This software is confidental and proprietary information
+* of Oleg Kiss. You shall not disclose such
+* confidental information and shall use it only in
+* accordance with terms of the license agreement
+* you entered into with Oleg Kiss.
+*/
+
+#ifndef SORTING_H_
+#define SORTING_H_
+
+typedef signed int SI;
+
+/*
+* @brief	This sort array in descending order 
+* @param	[in,out]		row - array
+* @param	[in]			rowSize - size of array
+* return	void
+*/
+
+void Sort(SI * row, SI rowSize);
+
+#endif /*SORTING_H_*/
