85ed652ff14dacb32420f7bae838c9163697e121
Oleg <kiss_home@mail.ru>
2012-06-26 23:54:37 +0300

    data input h

---
+++ task4.1/DataInput.h
@@ -0,0 +1,12 @@
+
+#ifndef DATAINPUT_H_
+#define DATAINPUT_H_
+
+#include <iostream>
+#include <fstream>
+
+void AD(std::istream & is, int * range, int * size);
+
+void AN(std::istream & is, int * num, int * size);
+
+#endif
