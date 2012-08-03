74c27a87b53e05b57d0818e46fc6427c7e47341e
Oleg <kiss_home@mail.ru>
2012-08-03 18:54:45 +0300

    template test

---
+++ task 8 List/8a.cpp
@@ -0,0 +1,13 @@
+// 8a.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include "List.h"
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	int r = 4;
+	List<int> a(r);
+	return 0;
+}
+
